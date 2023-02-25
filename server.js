0ed81da3834d2ea89fc0a525de5cecbe37b00fb9
brunocd2 <bruno.santos@cd2.com.br>
2023-02-25 15:03:51 -0300

    configure do express

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const path = require('path');
 const express = require('express');
-
+const {resolve} = require('path')
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 3000);
+app.use('/',
+        express.static(resolve(__dirname,
+          './build'
+          
+          ))
+)
 
-const server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
+app.listen(process.env.PORT || 3000, (err) =>{
+
+  if(err){return console.log(err)}
+  console.log('Tudo funcionando certinho ', server.address().port);
 });
